9ff439de78f98172cad33601d78b9fc55ec976be
payalpatel1208 <payalpatel12g@gmail.com>
2025-04-13 20:24:08 +0530

    array_practice

--- array_input.java
+++ array_input.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class array_input {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int[] arr = new int[5];
         for(int i=0; i < arr.length; i++){
             arr[i] = input.nextInt();
