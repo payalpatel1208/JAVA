16484fbd3caf32d2cd98521bc5690f3c7b212be2
payalpatel1208 <payalpatel12g@gmail.com>
2025-04-17 23:16:48 +0530

    2Darray_p
    today i am learn 2D array

---
+++ array2D.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+public class array2D {
+    public static void main(String[] args) {
+        int p,q,r,s;
+        Scanner input = new Scanner(System.in);
+        System.out.print ("Enter the number of rows of first metrix: ");
+        p = input.nextInt();
+        System.out.print("Enter the number of collums of first metrix: ");
+        q = input.nextInt();
+        System.out.print("Enter the number of rows of second metrix: ");
+        r = input.nextInt();
+        System.out.print("Enter the number of collums of first metrix: ");
+        s = input.nextInt();
+        if(p == r && q == s){
+            int a[][] = new int[p][q];
+            int b[][] = new int[r][s];
+            int c[][] = new int[r][s];
+            System.out.println("Enter all the elements of first matrix:");
+            for(int i = 0; i < p; i++)
+            {
+                for(int j = 0; j < q; j++)
+                {
+                    a[i][j] = input.nextInt();
+                }
+            }
+            System.out.println("");
+            System.out.println("Enter all the elements of second matrix:");
+            for(int i = 0; i < r; i++)
+            {
+                for(int j = 0; j < s; j++)
+                {
+                    b[i][j] = input.nextInt();
+                }
+            }
+            System.out.println("");
+            System.out.println("First Matrix");
+            for(int i = 0; i < p; i++)
+            {
+                for (int j= 0; j<q;j++)
+                {
+                    System.out.print(a[i][j]+"");
+                }
+                System.out.println("");
+            }
+            System.out.println("\nsecond Matrix");
+            for(int i = 0; i < r; i++)
+            {
+                for (int j= 0; j < s; j++)
+                {
+                    System.out.print(b[i][j]+"");
+                }
+                System.out.println("");
+            }
+            System.out.println("The sum of total number of matrix");
+            for(int i = 0; i < p; i++)
+            {
+                for(int j = 0; j < s; j++)
+                {
+                    for(int k = 0; k < q; k++)
+                    {
+                        c[i][j] = a[i][j]+b[i][j];
+                    }
+                }
+            }
+            for (int i = 0; i < p; i++) 
+            {
+                for (int j = 0; j < s; j++) 
+                {
+                    System.out.print(c[i][j]+" ");
+                }
+                System.out.println("");
+            }
+        }
+        else
+        {
+            System.out.println("Addition not possible");
+            System.out.println("Try Again");
+        }
+    }
+}
